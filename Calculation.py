157d6e4d05a554302c0ec815e4b58f43c054a2d0
yuvi155 <yuvrajpandey155@gmail.com>
2025-07-17 15:34:10 +0530

    I am doing chnages

---
+++ Calculation.py
@@ -0,0 +1,22 @@
+'''no = float(input("Enter a number: "))
+sq = no * no
+sqroot = no ** 0.5
+cube = no * no * no
+cuberoot = no ** (1/3)
+print("Square of number is:", sq)
+print("Square root of number is:", sqroot)
+print("Cube of number is:", cube)
+print("Cube root of number is:", cuberoot)
+'''
+
+
+no=int(input("Enter a number"))
+sq=no*no
+sqroot=no ** (1/2)
+cube=no*no*no
+cuberoot=no ** (1/3)
+
+print("Square of no is :",sq)
+print("Square root of no is :",sqroot)
+print("Cube of no is :",cube)
+print("Cube root of no is :",cuberoot)
